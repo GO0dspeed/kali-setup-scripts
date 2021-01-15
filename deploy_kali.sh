091a4e0d08a3ffce3a2a06d3387c005a23b030a6
Cory Goodspeed <cory.goodspeed@gmail.com>
2021-01-15 13:53:09 -0500

    Updates after testing

--- deploy_kali.sh
+++ deploy_kali.sh
@@ -14,37 +14,52 @@ fi
 
 # update apt repositories
 echo "Updating apt repos"
-apt-get update
-
-# Generate SSH Key
-
-echo "Generating SSH Key"
-ssh-keygen -q -N ""
+sudo apt-get update > /dev/null 2>&1
 
 # Install VS Code
 echo "Installing VS Code"
-apt-get install -y snapd && snap install --classic code
+sudo apt-get install -y snapd 
+sudo snap install --classic code
 
 # Install C2 Frameworks (Covenant and Merlin)
 echo "Beginning installation of Covenant and Merlin for C2..."
 echo "Installing .NET Core 3.1"
-wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg &&
-	mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/ &&
-	wget https://packages.microsoft.com/config/debian/9/prod.list && 
-	mv prod.list /etc/apt/sources.list.d/microsoft-prod.list && 
-	chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg&& 
-	chown root:root /etc/apt/sources.list.d/microsoft-prod.list 
+if [[ -f /etc/apt/trusted.gpg.d/microsoft.asc.gpg && /etc/apt/sources.list.d/microsoft-prod.list ]]
+then
+	echo "Microsoft package signing key already installed..."
+else
+	sudo wget -O - https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor > microsoft.asc.gpg &&
+		sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/ &&
+		sudo wget https://packages.microsoft.com/config/debian/9/prod.list && 
+		sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list && 
+		sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg&& 
+		sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list 
+fi
 sudo apt-get install -y apt-transport-https &&
-	sudo apt-get update && 
-	sudo apt-get install -y dotnet-sdk-5.0
+	sudo apt-get update > /dev/null 2>&1 && 
+	sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get update > /dev/null 2>&1 &&
+	sudo apt-get install -y apt-transport-https &&
+	sudo apt-get update > /dev/null 2>&1 &&
+	sudo apt-get install -y aspnetcore-runtime-3.1
 
 echo "Cloning Covenant to /opt/"
-cd /opt/ && 
-	git clone --recurse-submodules https://github.com/cobbr/Covenant
+if [[ -d /opt/Covenant ]]
+then
+	echo "Covenant already installed"
+else
+	cd /opt/ && 
+		sudo git clone --recurse-submodules https://github.com/cobbr/Covenant
+fi
 
 echo "Installing Merlin"
-cd /opt/ &&
-	mkdir merlin &&
-	cd merlin &&
-	wget https://github.com/Ne0nd0g/merlin/releases/download/v0.9.1-beta/merlinServer-Linux-x64.7z &&
-	7z x merlinServer-Linux-x64.7z -pmerlin
+if [[ -d /opt/merlin ]]
+then
+	echo "Merlin already installed"
+else
+	cd /opt/ &&
+		sudo mkdir merlin &&
+		sudo cd merlin &&
+		sudo wget https://github.com/Ne0nd0g/merlin/releases/download/v0.9.1-beta/merlinServer-Linux-x64.7z &&
+		sudo 7z x merlinServer-Linux-x64.7z -pmerlin
+fi
